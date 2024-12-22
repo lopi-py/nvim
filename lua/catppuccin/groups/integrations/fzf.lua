8877426f9db3b154aacd0e2f6c6a855a2efe685d
Lopy <70210066+lopi-py@users.noreply.github.com>
2024-12-22 13:30:57 -0500

    fix: improve fzf-lua integration

--- lua/catppuccin/groups/integrations/fzf.lua
+++ lua/catppuccin/groups/integrations/fzf.lua
@@ -2,9 +2,9 @@ local M = {}
 
 function M.get()
 	return {
-		FzfLuaNormal = { link = "NormalFloat" },
+		-- FzfLuaNormal = { link = "NormalFloat" },
+		-- FzfLuaTitle = { link = "FloatTitle" },
 		FzfLuaBorder = { link = "FloatBorder" },
-		FzfLuaTitle = { link = "FloatTitle" },
 		FzfLuaHeaderBind = { fg = C.yellow },
 		FzfLuaHeaderText = { fg = C.peach },
 		FzfLuaPathColNr = { fg = C.blue },
